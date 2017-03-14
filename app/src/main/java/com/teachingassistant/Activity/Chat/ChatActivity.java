f32b6aef8245d819f638215ed833ac3f42f87185
jianyunrui <934962377@qq.com>
2017-03-14 13:10:06 +0800

    把UI界面错误修改了一下

--- app/src/main/java/com/teachingassistant/Activity/Chat/ChatActivity.java
+++ app/src/main/java/com/teachingassistant/Activity/Chat/ChatActivity.java
@@ -10,6 +10,6 @@ public class ChatActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat);
+        setContentView(R.layout.activity_input);
     }
 }
